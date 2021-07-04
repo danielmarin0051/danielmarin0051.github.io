29e4156d04b5f53103537feddb44a134cf25f8e3
Daniel Marin <danielmarin0051@gmail.com>
2021-07-03 20:54:44 -0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="w-screen h-screen relative">
+    {/* <NavBar /> */}
+    <Component {...pageProps} />
+  </div>
 }
 export default MyApp
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      keyframes: {
+        "animation-fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fade-in-slow": "animation-fade-in 5s ease-in-out",
+        "fade-in": "animation-fade-in 3s ease-in-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

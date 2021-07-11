d12cc5dd677653abc6f4428a36f59c996d2a4c40
Daniel Marin <danielmarin0051@gmail.com>
2021-07-11 15:29:23 -0700

    Add og meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
+import Head from "next/head";
 import Typewriter from "typewriter-effect";
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Daniel Marin | Home</title>
+        <meta name="description" content="Hi, I'm Daniel." />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://www.danielmarin.dev/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Daniel Marin | Home" />
+        <meta property="og:description" content="Hi, I'm Daniel." />
+        <meta
+          property="og:image"
+          content="https://www.danielmarin.dev/images/opengraph.png"
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="danielmarin.dev" />
+        <meta property="twitter:url" content="https://www.danielmarin.dev/" />
+        <meta name="twitter:title" content="Daniel Marin | Home" />
+        <meta name="twitter:description" content="Hi, I'm Daniel." />
+        <meta
+          name="twitter:image"
+          content="https://www.danielmarin.dev/images/opengraph.png"
+        />
+      </Head>
       <iframe
         className="h-screen w-screen fixed top-0 left-0 animate-fade-in-slow overflow-hidden"
         src="/embeds/perlin-noise/index.html"
